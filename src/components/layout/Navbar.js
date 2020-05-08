a8f964f3068a34ea6d2783759bbfc65782653c27
Jason Lane Johnson <jasjohn3@uat.edu>
2020-05-08 12:51:49 -0700

    Navbar UI adjustment

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({ icon, title }) => {
   return (
-    <nav className="navbar bg-primary m-1">
+    <nav className="navbar bg-primary">
       <h1>
         <i className={icon} />
         {title}
